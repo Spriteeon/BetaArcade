6583ea595514bc8bb5f831aa691eb60476a2b701
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-10-22 15:04:32 +0100

    redid item base class and inventory component
    Last creation of classes broke the program. issue now fixed and classes re-implemented.

---
+++ Source/BetaArcade/Inventory/ItemBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, Blueprintable, EditInlineNew, DefaultToInstanced)
+class BETAARCADE_API UItemBase : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	
+};
