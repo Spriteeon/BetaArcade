fa42a5323f068913f4c823d81bb258a120a33fb2
MalCoward <60357765+MalCoward@users.noreply.github.com>
2021-01-07 01:54:09 +0000

    Test to make sure still open

--- Source/BetaArcade/BetaArcadeGameMode.cpp
+++ Source/BetaArcade/BetaArcadeGameMode.cpp
@@ -1,18 +1,18 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 #include "BetaArcadeGameMode.h"
-#include "BetaArcadeCharacter.h"
+//#include "BetaArcadeCharacter.h"
 #include "Math.h"
 #include "UObject/ConstructorHelpers.h"
 
 ABetaArcadeGameMode::ABetaArcadeGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	}*/
 
 	spawnedTiles = 0;
 }
